f5bd3ee8f36f672f41f46e1ca89ef590846edd9b
Kirill Chalyi <kirillchalyi99@gmail.com>
2019-08-26 13:15:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function (){
+	$("#close").click(function(){
+		$("#form").hide();
+	});
+});
+
+$(function (){
+	$(".all1").delay(0).show(1000);
+})
+
+$(function (){
+	$("#header4").hover(function(){
+	$(".all2").show(1000)});
+})
